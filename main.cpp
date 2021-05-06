9b7e8941c5c9462ff29f03a58be268f7f8aabd7b
SANGJO SIM <30883823+sjgod79@users.noreply.github.com>
2021-05-06 15:50:18 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -270,7 +270,7 @@ float MGRead(void)
     float v=0;
 
     for (i=0;i<READ_SAMPLE_TIMES;i++) {
-        v += ain*65535;
+        v += ain;
         // delay(READ_SAMPLE_INTERVAL);
         Thread::wait(1000);
         NODE_DEBUG("Analog Read : ");
@@ -278,7 +278,8 @@ float MGRead(void)
         NODE_DEBUG("  V    ");
 
     }
-    v = (v/READ_SAMPLE_TIMES) *5/1024 ;
+    //v = (v/READ_SAMPLE_TIMES) *5/1024 ;
+    v = (v/READ_SAMPLE_TIMES) *5/65536 ;
         NODE_DEBUG("SEN0159 : ");
         NODE_DEBUG("%f", v);
         NODE_DEBUG("  V    ");
